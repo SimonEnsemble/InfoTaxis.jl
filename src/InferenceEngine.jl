7a7f4b24ca97ec2d0f46927a129877aa8e513cca
morripau <morripau@oregonstate.edu>
2025-05-13 11:15:01 -0700

    added src file for MCMC sampling, Bayesian inference

---
+++ src/InferenceEngine.jl
@@ -0,0 +1,7 @@
+module InferenceEngine
+using LinearAlgebra, Turing, SpecialFunctions, DataFrames, StatsBase, Distributions, JLD2
+
+#############################################################################
+##  IMPORT DR. YANG'S MODEL DATA
+#############################################################################
+end
